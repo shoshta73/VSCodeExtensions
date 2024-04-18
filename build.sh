6cd52851eea575d45d1f2431925dda4a4f4ce40d
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-04-18 19:06:33 +0200

    updated some files

--- build.sh
+++ build.sh
@@ -1,5 +1,38 @@
+function install_extensions {
+    # Declare an array to hold the file names
+    declare -a files
+    
+    # Use find to search for .txt files and loop through the results
+    while IFS= read -r -d '' file; do
+        # Add each file to the array
+        files+=("$file")
+    done < <(find . -type f -name "*.vsix" -print0)
+    
+    # Print the array to verify the files were added
+    echo "Files found:"
+    for file in "${files[@]}"; do
+        code --install-extension $file
+    done
+}
+
 cd themes
 vsce package
 mv themes-0.0.1.vsix ../
 cd ..
 
+cd c-cpp
+vsce package
+mv c-cpp-0.0.1.vsix ../
+cd ..
+
+cd csharpdotnet
+vsce package
+mv csharpdotnet-0.0.1.vsix ../
+cd ..
+
+cd gocode
+vsce package
+mv gocode-0.0.1.vsix ../
+cd ..
+
+install_extensions
