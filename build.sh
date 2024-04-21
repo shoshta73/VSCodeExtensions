0c4b4ce8c05656ac322ae89d6fe9b3af65b43104
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-04-21 14:41:42 +0200

    added common extensions

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 function install_extensions {
     # Declare an array to hold the file names
     declare -a files
-    
     # Use find to search for .txt files and loop through the results
     while IFS= read -r -d '' file; do
         # Add each file to the array
         files+=("$file")
     done < <(find . -type f -name "*.vsix" -print0)
-    
     # Print the array to verify the files were added
     echo "Files found:"
     for file in "${files[@]}"; do
@@ -33,6 +31,11 @@ cd ..
 cd gocode
 vsce package
 mv gocode-0.0.1.vsix ../
+cd ..
+
+cd commons
+vsce package
+mv commons-0.0.1.vsix ../
 cd ..
 
 install_extensions
