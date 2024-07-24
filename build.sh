02e07984c8277149a7d2a17a6dddee359d842976
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-07-24 16:07:59 +0200

    update themes

--- build.sh
+++ build.sh
@@ -4,25 +4,10 @@ set -e
 
 DIRS=$(find . -maxdepth 1 -type d | grep -v '.git' | grep -v ".vscode" | grep -v "node_modules" | sed -e 's,^\.\/,,' | grep -v "\.")
 
-function check_git_changes() {
-    local result=$(git status . --porcelain)
-    echo "Git status result: $result" # Debugging echo
-    if [[ -n "$result" ]]; then
-        echo "There are changes in the current directory."
-        return 0
-    else
-        echo "No changes detected."
-        return 1
-    fi
-}
-
 for dir in $DIRS; do
     echo "Building $dir"
     cd $dir
-    npm version patch --no-git-tag-version
-    git add .
-    git commit -m"update"
-    git push
+    # npm version patch --no-git-tag-version
     ../node_modules/.bin/vsce package
     vsixFile=$(find . -maxdepth 1 -type f -name "*.vsix")
     echo $vsixFile
